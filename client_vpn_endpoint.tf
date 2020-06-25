62ca683c52c96df16cfb1d16b7470cb30c9d5374
Frédéric Notet <frederic@musimap.com>
2020-06-25 14:18:23 +0100

    vpn routes

--- client_vpn_endpoint.tf
+++ client_vpn_endpoint.tf
@@ -33,6 +33,7 @@ resource "aws_ec2_client_vpn_network_association" "client-vpn-network-associatio
 
 resource "null_resource" "authorize-client-vpn-ingress" {
   provisioner "local-exec" {
+    when = create
     command = "aws --region ${var.aws_region} ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --target-network-cidr 0.0.0.0/0 --authorize-all-groups"
   }
 
@@ -49,11 +50,11 @@ resource "null_resource" "client_vpn_route" {
   count = length(var.subnet_ids)
 
   provisioner "local-exec" {
-    when = "create"
+    when = create
     command = "aws ec2 create-client-vpn-route --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --destination-cidr-block 0.0.0.0/0 --target-vpc-subnet-id ${var.subnet_ids[count.index]} --description Internet-Access"
   }
   provisioner "local-exec" {
-    when = "destroy"
+    when = destroy
     command = "aws ec2  delete-client-vpn-route --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --destination-cidr-block 0.0.0.0/0"
   }
 
