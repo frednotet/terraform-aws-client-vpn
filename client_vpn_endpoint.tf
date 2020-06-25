69f22e68eda1b795992fc8057295314323df178a
Frédéric Notet <frederic@musimap.com>
2020-06-25 13:41:27 +0100

    client endpoint (hack for single client TBF)

---
+++ client_vpn_endpoint.tf
@@ -0,0 +1,31 @@
+resource "aws_ec2_client_vpn_endpoint" "client-vpn-endpoint" {
+  description            = "terraform-clientvpn-endpoint"
+  server_certificate_arn = aws_acm_certificate.server_cert.arn
+  client_cidr_block      = var.client_cidr_block
+
+  authentication_options {
+    type                       = "certificate-authentication"
+    root_certificate_chain_arn = aws_acm_certificate.clients_cert["fnotet"].arn
+  }
+
+  connection_log_options {
+    enabled = false
+  }
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.domain_name)
+    },
+    var.tags
+  )
+}
+
+
+resource "aws_ec2_client_vpn_network_association" "client-vpn-network-association" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id
+  subnet_id              = var.subnet_ids[0]
+
+  lifecycle {
+    ignore_changes = "subnet_id"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "aws_acm_certificate_server_arn" {
   value = aws_acm_certificate.server_cert.arn
 }
+
+output "aws_ec2_client_vpn_endpoint_id" {
+  value = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id
+}
