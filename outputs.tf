75b6ea0babb9b7cdf1e8bc6b4c3ca7414bb0b0a5
Frédéric Notet <frederic@musimap.com>
2020-06-25 13:08:04 +0100

    fix outputs cert

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "aws_acm_certificate_server_id" {
   value = aws_acm_certificate.server_cert.id
-
+}
 
 output "aws_acm_certificate_server_arn" {
   value = aws_acm_certificate.server_cert.arn
