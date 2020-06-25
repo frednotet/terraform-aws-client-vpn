6da2ca2e2370ede24d74f7ae7f417e520c47323a
Frédéric Notet <frederic@musimap.com>
2020-06-25 13:06:17 +0100

    outputs cert

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "aws_acm_certificate_server_id" {
+  value = aws_acm_certificate.server_cert.id
+
+
+output "aws_acm_certificate_server_arn" {
+  value = aws_acm_certificate.server_cert.arn
+}
