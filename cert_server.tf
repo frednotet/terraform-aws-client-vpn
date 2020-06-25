df087649f9c26a81a8df6a34a0a5d3cbaf71b696
Frédéric Notet <frederic@musimap.com>
2020-06-25 13:03:12 +0100

    cert_server

--- cert_server.tf
+++ cert_server.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "server_cert" {
-  private_key       = file("${path.root}/${var.cert_dir}/server.key")
-  certificate_body  = file("${path.root}/${var.cert_dir}/server.crt")
-  certificate_chain = file("${path.root}/${var.cert_dir}/ca.crt")
+  private_key       = file("${path.root}/${var.server_certs.private_key}")
+  certificate_body  = file("${path.root}/${var.server_certs.certificate_body}")
+  certificate_chain = file("${path.root}/${var.server_certs.certificate_chain}")
 }
